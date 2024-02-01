5e4efd21676bf8748f71096c890d675ef5640b61
Lezardo261 <lezardo261@gmail.com>
2024-02-01 07:25:03 +0700

    onprogress

---
+++ index.js
@@ -0,0 +1,40 @@
+// let nilai = [100, 80, 80, 70, 90];
+
+// // Buat kode kamu di bawah ini
+// let hasil = nilai.reduce((total, item) => {
+//     return total + item;
+// });
+
+// let jumlahNilai = nilai.reduce((total, item) => {
+//     return total + item;
+// });
+
+// let rataRata = jumlahNilai/nilai.length;
+
+// console.log(rataRata);
+
+// let angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// // Buat kode kamu di bawah ini
+
+// let number = angka.filter((item) =>{
+//     return item%2 === 0;
+// });
+
+// let genap = angka.filter((item) =>{
+//     return item%2 === 0;
+// });
+
+
+// console.log(genap);
+
+// // Perbaiki kode berikut
+// let hewan = ["Ayam", "Domba", "Ayam", "Rubah"];
+
+// let indexHewan = hewan.indexOf("Ayam");
+
+// let indexAyam = hewan.indexOf("Ayam");
+
+// console.log(indexAyam);
+
+
